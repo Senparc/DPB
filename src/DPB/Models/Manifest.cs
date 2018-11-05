57240cd3416d1ef1bb7eedb5e46204cf0ab3bf26
Jeffrey Su <zsu@senparc.com>
2018-11-05 23:43:52 +0800

    add DPB source project

---
+++ src/DPB/Models/Manifest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPB.Models
+{
+    /// <summary>
+    /// Manifest module
+    /// </summary>
+    public class Manifest
+    {
+        public List<Config> Configs { get; set; }
+    }
+
+    /// <summary>
+    /// One config group
+    /// </summary>
+    public class Config
+    {
+        /// <summary>
+        /// find the paths in this config group
+        /// </summary>
+        public List<string> Paths { get; set; }
+
+    }
+
+
+}
