f5bbd9898b1342d4b006fbdd72c3beb89014307f
Jeffrey Su <zsu@senparc.com>
2018-11-06 19:47:27 +0800

    version v0.1.0.0

--- src/DPB/Properties/AssemblyInfo.cs
+++ src/DPB/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("DPB")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("DPB - Dynamic Project Builder")]
+[assembly: AssemblyDescription("DPB is a tool that allows developers to automatically generate project code.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Senparc")]
 [assembly: AssemblyProduct("DPB")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
