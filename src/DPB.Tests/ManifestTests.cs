da935fdacffb84ee2e3a2a36e1af9f84bac67a42
Jeffrey Su <zsu@senparc.com>
2018-11-06 13:45:05 +0800

    完善单元测试

--- src/DPB.Tests/ManifestTests.cs
+++ src/DPB.Tests/ManifestTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using DPB.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Senparc.CO2NET.Extensions;
@@ -30,6 +31,20 @@ namespace DPB.Tests
             Console.WriteLine(dpbManifest.ToJson());
         }
 
+        [TestMethod]
+        public void MyTestMethod()
+        {
+            Manifest manifest = new Manifest();
+            manifest.SourceDir = "";
+            manifest.OutputDir = "";
+            manifest.Paths.Add(new PathConfig()
+            {
+                Files = new List<string>() { "*.cs" },
+            });
 
+            LetsGo letsGo = new LetsGo(manifest);
+
+            letsGo.Build();
+        }
     }
 }
---
+++ src/DPB/LetsGo.cs
@@ -0,0 +1,24 @@
+﻿using DPB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPB
+{
+    public class LetsGo
+    {
+        public Manifest Manifest { get; set; }
+
+        public LetsGo(Manifest manifest)
+        {
+            Manifest = manifest;
+        }
+
+        public void Build()
+        {
+
+        }
+    }
+}
