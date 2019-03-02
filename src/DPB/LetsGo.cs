f1df5eff3985be798e33c4d1e1000aab4156b52a
Jeffrey Su <zsu@senparc.com>
2019-03-02 11:37:25 +0800

    modify

--- src/DPB/LetsGo.cs
+++ src/DPB/LetsGo.cs
@@ -15,7 +15,7 @@ using Newtonsoft.Json.Linq;
 
 namespace DPB
 {
-    public class LetsGo
+    public partial class LetsGo
     {
         public Manifest Manifest { get; set; }
 
@@ -363,6 +363,7 @@ namespace DPB
                         {
                             Record($"[in memory] remove directory: {dirPath}");
                             FilesCache.Remove(file);
+                            files.Remove(file);
                         }
                     }
 
@@ -599,22 +600,32 @@ namespace DPB
                 #region save new files
                 Parallel.ForEach(FilesCache.Values, /*new ParallelOptions() { MaxDegreeOfParallelism = 30 },*/ fileWrap =>
                     {
-                        var dir = fileWrap.DestFilePath.Substring(0, fileWrap.DestFilePath.LastIndexOf(Path.DirectorySeparatorChar));
-                        if (!Directory.Exists(dir))
+                        try
                         {
-                            Directory.CreateDirectory(dir);
+                            var dir = fileWrap.DestFilePath.Substring(0, fileWrap.DestFilePath.LastIndexOf(Path.DirectorySeparatorChar));
+                            if (!Directory.Exists(dir))
+                            {
+                                Directory.CreateDirectory(dir);
+                            }
+
+                            //save the file to OutputDir
+                            var openMode = File.Exists(fileWrap.DestFilePath) ? FileMode.Truncate : FileMode.Create;
+                            using (var fs = new FileStream(fileWrap.DestFilePath, openMode))
+                            {
+                                var sw = new StreamWriter(fs, Encoding.UTF8);
+                                sw.Write(fileWrap.FileContent);
+                                sw.Flush();
+                                fs.Flush(true);
+                                Record($"modified and saved a new file: {fileWrap.DestFilePath}");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Record($@"saved a file error: {fileWrap.DestFilePath}
+{ex.Message}
+{ex.StackTrace}");
                         }
 
-                        //save the file to OutputDir
-                        var openMode = File.Exists(fileWrap.DestFilePath) ? FileMode.Truncate : FileMode.Create;
-                        using (var fs = new FileStream(fileWrap.DestFilePath, openMode))
-                        {
-                            var sw = new StreamWriter(fs, Encoding.UTF8);
-                            sw.Write(fileWrap.FileContent);
-                            sw.Flush();
-                            fs.Flush(true);
-                            Record($"modified and saved a new file: {fileWrap.DestFilePath}");
-                        }
                     });
                 #endregion
 
@@ -650,289 +661,5 @@ namespace DPB
             }
         }
 
-        #region 异步方法
-
-        ///// <summary>
-        ///// Build a new project from source
-        ///// </summary>
-        ///// <param name="cleanOutputDir"></param>
-        //public async Task BuildAsync(bool cleanOutputDir = true)
-        //{
-        //    var startTime = DateTime.Now;
-        //    Records.Clear();
-        //    Record($"---- DBP Build begin at {startTime.ToString()}  ----");
-
-        //    //var fullSourceRoot = Path.Combine(Directory.GetCurrentDirectory(), Manifest.SourceDir);
-        //    //var fullOutputRoot = Path.Combine(Directory.GetCurrentDirectory(), Manifest.OutputDir);
-
-        //    if (cleanOutputDir)
-        //    {
-        //        try
-        //        {
-        //            Directory.Delete(Manifest.AbsoluteOutputDir, true);
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            SenparcTrace.BaseExceptionLog(ex);
-        //        }
-        //    }
-
-        //    if (!Directory.Exists(Manifest.AbsoluteOutputDir))
-        //    {
-        //        Directory.CreateDirectory(Manifest.AbsoluteOutputDir);
-        //    }
-
-        //    //Copy all files
-        //    Record($"===== start copy all files  =====");
-        //    CopyDirectory(Manifest.AbsoluteSourceDir, Manifest.AbsoluteOutputDir);
-        //    Record($"---- end copy all files  ----");
-        //    Record($"---- {(DateTime.Now - startTime).TotalSeconds} seconds  ----");
-
-
-        //    int groupIndex = 0;
-
-        //    var filesGroup = new List<List<string>>();
-        //    foreach (var configGroup in Manifest.ConfigGroup)
-        //    {
-        //        var omitFiles = configGroup.OmitFiles.SelectMany(f => Directory.GetFiles(Manifest.AbsoluteOutputDir, f, SearchOption.AllDirectories)).ToList();
-        //        var files = configGroup.Files.SelectMany(f => Directory.GetFiles(Manifest.AbsoluteOutputDir, f, SearchOption.AllDirectories))
-        //                        .Where(f => !omitFiles.Contains(f)).ToList();
-        //        filesGroup.Add(files);
-        //        AllFilesCount += files.Count;
-        //    }
-
-        //    foreach (var configGroup in Manifest.ConfigGroup)
-        //    {
-        //        groupIndex++;
-
-        //        Record($"config group: {groupIndex}");
-
-        //        var omitFiles = configGroup.OmitFiles.SelectMany(f => Directory.GetFiles(Manifest.AbsoluteOutputDir, f, SearchOption.AllDirectories)).ToList();
-        //        var files = filesGroup[groupIndex - 1];
-
-        //        #region Remove Files
-
-        //        if (configGroup.RemoveFiles)
-        //        {
-        //            Record($"remove files:");
-        //            foreach (var file in files)
-        //            {
-        //                File.Delete(file);
-        //                Record($"removed file: {file}");
-        //                CheckAndRemoveEmptyDirectory(file);
-        //                FinishedFilesCount++;
-        //            }
-        //            continue;
-        //        }
-
-        //        #endregion
-
-        //        #region Remove Directories
-
-        //        foreach (var dir in configGroup.RemoveDictionaries)
-        //        {
-        //            var dirPath = Path.Combine(Manifest.AbsoluteOutputDir, dir);
-        //            Record($"tobe remove directory: {dirPath}");
-        //            if (Directory.Exists(dirPath))
-        //            {
-        //                Record($"remove directory: {dirPath}");
-        //                Directory.Delete(dirPath, true);
-        //            }
-        //        }
-
-        //        #endregion
-
-        //        foreach (var file in files)
-        //        {
-        //            Record($"dynamic file: {file}");
-
-        //            var newContent = new StringBuilder();
-        //            string fileContent = null;
-        //            using (var fs = new FileStream(file, FileMode.Open))
-        //            {
-        //                using (var sr = new StreamReader(fs, Encoding.UTF8))
-        //                {
-        //                    fileContent = await sr.ReadToEndAsync();
-        //                }
-        //            }
-
-        //            #region File Mark
-
-        //            if (fileContent.Contains(FILE_MARK_PREFIX))
-        //            {
-        //                //judgement whether this file can keep
-        //                var regex = new Regex($@"{FILE_MARK_PREFIX}(?<kw>[^\r\n \*,]*)");
-        //                var match = regex.Match(fileContent);
-        //                if (match.Success && !configGroup.KeepFileConiditions.Any(z => z == match.Groups["kw"].Value))
-        //                {
-        //                    //remove this file
-        //                    Record($"remove this file");
-        //                    try
-        //                    {
-        //                        File.Delete(file);
-        //                        Record($"removed file: {file}");
-        //                        CheckAndRemoveEmptyDirectory(file);
-        //                    }
-        //                    catch (Exception ex)
-        //                    {
-        //                        Record($"delete file error:{ex}");
-        //                    }
-        //                    continue;
-        //                }
-        //            }
-
-        //            #endregion
-
-        //            #region ReplaceContents
-
-        //            XDocument xml = null;
-        //            dynamic json = null;
-        //            foreach (var replaceContent in configGroup.ReplaceContents)
-        //            {
-        //                #region Xml
-
-        //                if (replaceContent.XmlContent != null)
-        //                {
-        //                    try
-        //                    {
-        //                        xml = xml ?? XDocument.Parse(fileContent);
-        //                        ReplaceXmlElements(xml.Root, replaceContent.XmlContent);
-        //                    }
-        //                    catch (Exception ex)
-        //                    {
-        //                        Record($"Xml file format wrong");
-        //                        SenparcTrace.SendCustomLog("Xml file format wrong", ex.Message);
-        //                    }
-        //                }
-        //                #endregion
-
-        //                #region Json
-        //                else if (replaceContent.JsonContent != null)
-        //                {
-        //                    try
-        //                    {
-        //                        //var serializer = new JavaScriptSerializer();
-        //                        json = serializer.Deserialize(fileContent, typeof(object));
-        //                        ReplaceJsonNodes(json, replaceContent.JsonContent);
-        //                    }
-        //                    catch (Exception ex)
-        //                    {
-        //                        Record($"Json file format wrong");
-        //                        SenparcTrace.SendCustomLog("Json file format wrong", ex.Message);
-        //                    }
-        //                }
-
-        //                #endregion
-        //            }
-
-        //            if (xml != null)
-        //            {
-        //                fileContent = xml.ToString();
-        //                Record($"Xml file changed.");
-        //            }
-        //            else if (json != null)
-        //            {
-        //                fileContent = JsonConvert.SerializeObject(json);
-        //                Record($"Json file changed.");
-        //            }
-
-
-        //            #endregion
-
-        //            #region Content Mark
-
-        //            if (fileContent.Contains(BEGIN_MARK_PERFIX))
-        //            {
-        //                var lines = fileContent.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
-        //                var keep = true;
-        //                var removeBlockCount = 0;
-        //                var i = 0;
-        //                foreach (var line in lines)
-        //                {
-        //                    i++;
-        //                    if (keep)
-        //                    {
-        //                        if (line.Contains(BEGIN_MARK_PERFIX))
-        //                        {
-        //                            //begin to check Conditions
-        //                            if (!configGroup.KeepContentConiditions.Any(z => line.Contains(z)))
-        //                            {
-        //                                //drop content
-        //                                keep = false;
-        //                                removeBlockCount++;
-        //                                continue;
-        //                            }
-        //                        }
-
-        //                        //keep
-        //                        newContent.Append(line);
-        //                        if (i != lines.Count())
-        //                        {
-        //                            newContent.Append(Environment.NewLine);   //not last Item
-        //                        }
-        //                    }
-        //                    else
-        //                    {
-        //                        //not keep, waiting the end mark
-        //                        if (line.Contains(END_MARK))
-        //                        {
-        //                            keep = true;
-        //                        }
-        //                    }
-        //                }
-        //            }
-        //            else
-        //            {
-        //                newContent.Append(fileContent);
-        //            }
-
-        //            #endregion
-
-        //            #region save new file
-
-        //            //save the file to OutputDir
-        //            using (var fs = new FileStream(file, FileMode.Truncate))
-        //            {
-        //                var sw = new StreamWriter(fs, Encoding.UTF8);
-        //                await sw.WriteAsync(newContent.ToString());
-        //                await sw.FlushAsync();
-        //                fs.Flush(true);
-        //                Record($"modified and saved a new file: {file}");
-        //            }
-
-        //            #endregion
-
-        //            FinishedFilesCount++;
-        //        }
-        //    }
-
-        //    var manifestFileName = Path.Combine(Manifest.AbsoluteOutputDir, "manifest.config");
-        //    using (var logFs = new FileStream(manifestFileName, FileMode.Create))
-        //    {
-        //        var sw = new StreamWriter(logFs, Encoding.UTF8);
-        //        await sw.WriteAsync(Manifest.ToJson());
-        //        await sw.FlushAsync();
-        //        logFs.Flush(true);
-        //    }
-        //    Record($"saved manifest file: {manifestFileName}");
-
-
-        //    var logFileName = Path.Combine(Manifest.AbsoluteOutputDir, "DPB.log");
-        //    Record($"saved log file: {logFileName}");
-        //    Record($"===== DPB Build Finished =====");
-        //    Record($"---- Total time: {(DateTime.Now - startTime).TotalSeconds} seconds ----");
-
-        //    using (var logFs = new FileStream(logFileName, FileMode.Create))
-        //    {
-        //        var sw = new StreamWriter(logFs, Encoding.UTF8);
-        //        var logs = new StringBuilder();
-        //        Records.ForEach(z => logs.AppendLine(z));
-        //        await sw.WriteAsync(logs.ToString());
-        //        await sw.FlushAsync();
-        //        logFs.Flush(true);
-        //    }
-        //}
-
-        #endregion
     }
 }
---
+++ src/DPB/LetsGoAsync.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPB
+{
+    public partial class LetsGo
+    {
+
+        #region Asynchronous Method
+
+
+        #endregion
+    }
+}
